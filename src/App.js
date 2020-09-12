f61d4be6dda4ac7fa7e2e6adaa26019532293180
Ryan Kim <iawal86@gmail.com>
2020-09-12 08:38:44 -0700

    created header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 
+import Header from "./components/Header";
+
 import "./App.css";
 
 function App() {
   return (
     <div className="app">
-      <h1>Hello</h1>
+      <Header />
     </div>
   );
 }
